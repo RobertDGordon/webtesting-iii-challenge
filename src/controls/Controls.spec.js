e8bcb9e8cfe84c181039322f4c27896b1a2d345f
Robert Gordon <Komeuppance@gmail.com>
2019-12-15 18:11:42 -0800

    First control test complete

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,14 @@
-// Test away!
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import Controls from './Controls'
+import { render, fireEvent } from '@testing-library/react'
+import expectExport from 'expect'
+
+test('closes gate when clicked', () =>{
+    const toggleClosed = jest.fn()
+    const {getByText} = render(<Controls locked={false} closed={false} toggleClosed={toggleClosed} /> )
+
+    const gateBtn = getByText(/close gate/i)
+    fireEvent.click(gateBtn);
+    expect(toggleClosed).toHaveBeenCalled();
+})
