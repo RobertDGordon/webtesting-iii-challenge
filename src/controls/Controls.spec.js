f558061faf0e6260f3bd42e4bb784bc2dae77f20
Robert Gordon <Komeuppance@gmail.com>
2019-12-15 18:18:57 -0800

    Toggle open gate test complete

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -2,13 +2,21 @@ import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import Controls from './Controls'
 import { render, fireEvent } from '@testing-library/react'
-import expectExport from 'expect'
 
 test('closes gate when clicked', () =>{
     const toggleClosed = jest.fn()
     const {getByText} = render(<Controls locked={false} closed={false} toggleClosed={toggleClosed} /> )
 
     const gateBtn = getByText(/close gate/i)
+    fireEvent.click(gateBtn);
+    expect(toggleClosed).toHaveBeenCalled();
+})
+
+test('opens gate when clicked', () =>{
+    const toggleClosed = jest.fn()
+    const {getByText} = render(<Controls locked={false} closed={true} toggleClosed={toggleClosed} /> )
+
+    const gateBtn = getByText(/open gate/i)
     fireEvent.click(gateBtn);
     expect(toggleClosed).toHaveBeenCalled();
 })
